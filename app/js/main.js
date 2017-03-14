a0cce3b6f229ecd9d22132a83c58024dd0b44ded
moduldev <modul.work@gmail.com>
2017-03-14 18:21:37 +0200

    js accodiation

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,7 @@
-/**
- * Created by Сергей on 13.03.2017.
- */
+
+$(document).ready(function (e) {
+   var trigger = $('.fa-plus');
+    $(trigger).click(function () {
+       $('.widget_wrapper').css('height','50px')
+    });
+});
