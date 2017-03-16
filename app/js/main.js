333c9f2df2259d81f0e41df634b6d83eebbcb392
moduldev <modul.work@gmail.com>
2017-03-16 07:53:31 +0200

    15.03.17

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,40 @@
 
-$(document).ready(function (e) {
-   var trigger = $('.fa-plus');
-    $(trigger).click(function () {
-       $('.widget_wrapper').css('height','50px')
+// $(document).on('click', '.widget_wrapper', function () {
+//     var that = $('.widget_container');
+//     that.toggleClass('open');
+//     that.next().slideToggle('fast');
+// });
+
+$(function ($) {
+    $('.widget_wrapper').click(function () {
+        $(this).children('.widget_container').slideToggle();
+        $(this).children('.fa-plus').toggleClass('hide');
+        $(this).children('.fa-minus').toggleClass('show');
+    })
+});
+
+$(document).ready(function() {
+    var stickyNavTop = $('#sticky_box').height();
+    var stickyNav = function(){
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop > stickyNavTop) {
+
+            $('#sticky_box').addClass('sticky');
+            $('#logo_box').addClass('sticky_header_logo_box').removeClass('header_logo_box');
+            $('#custom_heater_form').addClass('custom_heater_form_sticky');
+
+
+        } else {
+            $('#sticky_box').removeClass('sticky') ;
+            $('#logo_box').addClass('header_logo_box').removeClass('sticky_header_logo_box');
+            $('.custom_heater_form').css('margin','11px 0 16px 0');
+            $('.mega_menu_hover_line:hover:after').css('bottom','0');
+        }
+    };
+
+    stickyNav();
+
+    $(window).scroll(function() {
+        stickyNav();
     });
 });
